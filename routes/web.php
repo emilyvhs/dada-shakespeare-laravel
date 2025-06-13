e970a50061b42c83af8c0817c0bf177f3edf7534
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-06-13 10:28:22 +0100

    Redundant (non API) routes and controllers removed

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,2 @@
 <?php
 
-use App\Http\Controllers\ChapterController;
-use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ParagraphController;
-use App\Http\Controllers\WorkController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [HomeController::class, 'home']);
-
-Route::get('/plays', [WorkController::class, 'allPlays']);
-Route::get('/plays/{work:WorkID}', [WorkController::class, 'find']);
-
-Route::get('/chapters', [ChapterController::class, 'all']);
-
-Route::get('/characters', [CharacterController::class, 'all']);
-
-Route::get('/genres', [GenreController::class, 'all']);
-
-Route::get('/paragraphs', [ParagraphController::class, 'all']);
