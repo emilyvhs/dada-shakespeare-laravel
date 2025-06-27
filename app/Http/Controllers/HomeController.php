ff20b7e32b630af7ce9db4bea51e19b4332bfa79
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-06-27 11:49:58 +0100

    Adjusting query in HomeController re characters for consistency with CharacterApiController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,8 +15,6 @@ class HomeController extends Controller
 
         //retrieve all characters
         $characters = DB::table('Characters')
-            //exclude any characters who do not speak
-            ->where('SpeechCount', '>', 0)
             //exclude CharNames that refer to groups of already listed characters/stage directions
             ->whereNotIn('CharName', ['All', 'All Citizens', 'All Conspirators', 'All Ladies', 'All Lords', 'All Servants', 'All The People', 'Another', 'Both', 'Both Citizens', 'Both Tribunes', 'Brothers', 'Several Citizens', 'Some Speak', '(stage directions)'])
             ->get();
