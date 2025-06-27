423c71fb7e67565f721e7f88038beb55fabae6f4
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-06-27 12:14:13 +0100

    Updated results so character list appears in alphabetical order

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -43,7 +43,6 @@ class ResultsController extends Controller
             ->orWhere('CharID', '=', $addedCharacter)
             //exclude CharNames that refer to groups of already listed characters/stage directions
             ->whereNotIn('CharName', ['All', 'All Citizens', 'All Conspirators', 'All Ladies', 'All Lords', 'All Servants', 'All The People', 'Another', 'Both', 'Both Citizens', 'Both Tribunes', 'Brothers', 'Several Citizens', 'Some Speak', '(stage directions)'])
-            ->orderBy('SpeechCount', 'desc')
             ->get();
 
         //if shuffling every line...
